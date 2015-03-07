6fb3cb864b9df5f12ca32f9d5501b3c50859c465
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-07 17:26:31 +0900

    Add configuration spec

--- spec/Configuration.spec.php
+++ spec/Configuration.spec.php
@@ -4,11 +4,30 @@ use expect\Configuration;
 use Assert\Assertion;
 
 describe('Configuration', function() {
+    beforeEach(function() {
+        $this->config = Configuration::loadFromFile(__DIR__ . '/fixtures/config.toml');
+    });
     describe('#loadFromFile', function() {
-        beforeEach(function() {
-            $this->config = Configuration::loadFromFile(__DIR__ . '/fixtures/config.toml');
-        });
         it('return Configuration instance', function() {
+            Assertion::isInstanceOf($this->config, 'expect\Configuration');
+        });
+    });
+    describe('#getResultReporter', function() {
+        it('return expect\ResultReporter', function() {
+            Assertion::isInstanceOf($this->config->getResultReporter(), 'expect\ResultReporter');
+        });
+    });
+    describe('#getMatcherPackages', function() {
+        beforeEach(function() {
+            $this->matcherPackages = $this->config->getMatcherPackages();
+        });
+        it('return matcher packages', function() {
+            Assertion::isArray($this->matcherPackages);
+        });
+        describe('when have one package', function() {
+            it('return one package', function() {
+                Assertion::count($this->matcherPackages, 1);
+            });
         });
     });
 });
