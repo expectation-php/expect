57b71222e7ecdfba7ff43346263141e7e3f0cc37
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-07 17:35:05 +0900

    Add exception spec

--- spec/Configuration.spec.php
+++ spec/Configuration.spec.php
@@ -1,7 +1,9 @@
 <?php
 
 use expect\Configuration;
+use expect\config\ConfigurationFileNotFoundException;
 use Assert\Assertion;
+
 
 describe('Configuration', function() {
     beforeEach(function() {
@@ -10,6 +12,18 @@ describe('Configuration', function() {
     describe('#loadFromFile', function() {
         it('return Configuration instance', function() {
             Assertion::isInstanceOf($this->config, 'expect\Configuration');
+        });
+        context('when config file not found', function() {
+            it('throw ConfigurationFileNotFoundException', function() {
+                $throwExpection = false;
+
+                try {
+                    Configuration::loadFromFile(__DIR__ . '/fixtures/not_found_config.toml');
+                } catch (ConfigurationFileNotFoundException $exception) {
+                    $throwExpection = true;
+                }
+                Assertion::true($throwExpection);
+            });
         });
     });
     describe('#getResultReporter', function() {
@@ -24,7 +38,7 @@ describe('Configuration', function() {
         it('return matcher packages', function() {
             Assertion::isArray($this->matcherPackages);
         });
-        describe('when have one package', function() {
+        context('when have one package', function() {
             it('return one package', function() {
                 Assertion::count($this->matcherPackages, 1);
             });
