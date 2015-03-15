97433b1a946508c49af8433ef88cb9fe24ef77f5
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-15 18:11:29 +0900

    add ToStartWith spec

---
+++ src/matcher/ComparePattern.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of expect package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expect\matcher;
+
+use expect\FailedMessage;
+
+
+trait ComparePattern
+{
+
+    /**
+     * @var \expect\matcher\ToMatch
+     */
+    private $patternMatcher;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match($actual)
+    {
+        return $this->patternMatcher->match($actual);
+    }
+
+}
