566f368186704be82f081da924ff3daf0ec9b9f4
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-15 17:54:14 +0900

    Add toMatch

---
+++ spec/matcher/ToMatch.spec.php
@@ -0,0 +1,52 @@
+<?php
+
+use expect\matcher\ToMatch;
+use expect\FailedMessage;
+use Assert\Assertion;
+
+
+describe('ToMatch', function() {
+
+    describe('#match', function() {
+        beforeEach(function() {
+            $this->matcher = new ToMatch('/foo/');
+        });
+        context('when match', function() {
+            it('return true', function() {
+                $result = $this->matcher->match('foo');
+                Assertion::true($result);
+            });
+        });
+        context('when unmatch', function() {
+            it('return false', function() {
+                $result = $this->matcher->match('bar');
+                Assertion::false($result);
+            });
+        });
+    });
+
+    describe('#reportFailed', function() {
+        beforeEach(function() {
+            $this->matcher = new ToMatch('/foo/');
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match('bar');
+            $this->matcher->reportFailed($this->message);
+            Assertion::same((string) $this->message, "\nexpected 'bar' to match /foo/\n");
+        });
+    });
+
+    describe('#reportNegativeFailed', function() {
+        beforeEach(function() {
+            $this->matcher = new ToMatch('/foo/');
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match('foo');
+            $this->matcher->reportNegativeFailed($this->message);
+            Assertion::same((string) $this->message, "\nexpected 'foo' not to match /foo/\n");
+        });
+    });
+
+});
---
+++ src/matcher/ToMatch.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of expect package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace expect\matcher;
+
+
+use expect\Matcher;
+use expect\FailedMessage;
+
+
+final class ToMatch implements Matcher
+{
+
+    private $actual;
+    private $expected;
+
+    public function __construct($expected)
+    {
+        $this->expected = $expected;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match($actual)
+    {
+        $this->actual = $actual;
+        return (preg_match($this->expected, $this->actual) === 1);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reportFailed(FailedMessage $message)
+    {
+        $message->appendText('expected ')
+            ->appendValue($this->actual)
+            ->appendText(" to match ")
+            ->appendText($this->expected);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reportNegativeFailed(FailedMessage $message)
+    {
+        $message->appendText('expected ')
+            ->appendValue($this->actual)
+            ->appendText(" not to match ")
+            ->appendText($this->expected);
+    }
+
+}
