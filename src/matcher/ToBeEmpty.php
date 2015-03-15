2f92a8ff386cbe5f28e3e28a0f30bacf8ab8c323
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-15 17:41:52 +0900

    Add to be empty matcher

---
+++ src/matcher/ToBeEmpty.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of expect package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace expect\matcher;
+
+
+use expect\Matcher;
+
+
+final class ToBeEmpty implements Matcher
+{
+
+    use CompareLength;
+
+    public function __construct()
+    {
+        $this->lengthMatcher = new ToHaveLength(0);
+    }
+
+}
