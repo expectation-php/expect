41bf639ed91f06065f00b1e1349d96b9d70e2af6
holyshared <holy.shared.design@gmail.com>
2015-03-16 00:42:05 +0000

    Add dot reporter

--- peridot.php
+++ peridot.php
@@ -2,7 +2,9 @@
 
 use Evenement\EventEmitterInterface;
 use cloak\peridot\CloakPlugin;
+use Peridot\Reporter\Dot\DotReporterPlugin;
 
 return function(EventEmitterInterface $emitter) {
     CloakPlugin::create('cloak.toml')->registerTo($emitter);
+    (new DotReporterPlugin($emitter));
 };
