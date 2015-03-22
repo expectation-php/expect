f365a39025c5d3d81c610e160904ea4a16673599
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-22 17:21:51 +0900

    add print

---
+++ example/print.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace expect\example;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use expect\Expect;
+use expect\configurator\FileConfigurator;
+
+$configurator = new FileConfigurator(__DIR__ . '/config.toml');
+Expect::configure($configurator);
+
+
+Expect::that(function () {
+    echo 'foo';
+})->toPrint('foo'); //pass
+
+Expect::that(function () {
+    echo 'bar';
+})->toPrint('foo'); //failed
