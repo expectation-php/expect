9f339d7f010b44c0a22b37d68d3027d016b9e7a2
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-23 00:24:22 +0900

    Add interface

---
+++ src/matcher/ReportableMatcher.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of expect package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expect\matcher;
+
+use expect\Matcher;
+
+
+interface ReportableMatcher extends Matcher
+{
+}
