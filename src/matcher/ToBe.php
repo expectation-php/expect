50f8353b1ee293c17b7c5c2372fe70e694e2bbc7
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-23 00:03:16 +0900

    Add alias matcher

---
+++ src/matcher/ToBe.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of expect package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expect\matcher;
+
+
+use expect\Matcher;
+
+
+final class ToBe implements Matcher
+{
+
+    use CompareEqual;
+
+    public function __construct($expected)
+    {
+        $this->equalMatcher = new ToEqual($expected);
+    }
+
+}
