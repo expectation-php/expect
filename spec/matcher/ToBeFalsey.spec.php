5d5e23799bcf4b4bdd9536df0cc5a2e4a1f27218
holyshared <holy.shared.design@gmail.com>
2015-03-23 01:06:18 +0000

    Add FalseyMatcher

---
+++ spec/matcher/ToBeFalsey.spec.php
@@ -0,0 +1,66 @@
+<?php
+
+use expect\matcher\ToBeFalsey;
+use expect\FailedMessage;
+use Assert\Assertion;
+
+
+describe('ToBeFalsey', function() {
+
+    describe('#match', function() {
+        beforeEach(function() {
+            $this->matcher = new ToBeFalsey();
+        });
+        context('when match', function() {
+            context('when actual is false', function() {
+                it('return true', function() {
+                    Assertion::true($this->matcher->match(false));
+                });
+            });
+            context('when actual is null', function() {
+                it('return true', function() {
+                    Assertion::true($this->matcher->match(null));
+                });
+            });
+        });
+        context('when unmatch', function() {
+            context('when actual is true', function() {
+                it('return false', function() {
+                    Assertion::false($this->matcher->match(true));
+                });
+            });
+            context('when actual is blank', function() {
+                it('return false', function() {
+                    Assertion::false($this->matcher->match(''));
+                });
+            });
+            context('when actual is 0', function() {
+                it('return false', function() {
+                    Assertion::false($this->matcher->match(0));
+                });
+            });
+        });
+    });
+    describe('#reportFailed', function() {
+        beforeEach(function() {
+            $this->matcher = new ToBeFalsey();
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match(true);
+            $this->matcher->reportFailed($this->message);
+            Assertion::same((string) $this->message, "\nExpected falsey value, got true\n");
+        });
+    });
+    describe('#reportNegativeFailed', function() {
+        beforeEach(function() {
+            $this->matcher = new ToBeFalsey();
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match(false);
+            $this->matcher->reportNegativeFailed($this->message);
+            Assertion::same((string) $this->message, "\nExpected truthy value, got false\n");
+        });
+    });
+});
---
+++ src/matcher/ToBeFalsey.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+  * This file is part of expect package.
+  *
+  * (c) Noritaka Horio <holy.shared.design@gmail.com>
+  *
+  * This source file is subject to the MIT license that is bundled
+  * with this source code in the file LICENSE.
+  */
+
+
+namespace expect\matcher;
+
+
+use expect\Matcher;
+use expect\FailedMessage;
+
+
+final class ToBeFalsey implements Matcher
+{
+
+    /**
+     * @var mixed
+     */
+    private $actual;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match($actual)
+    {
+        return $this->matchFalsey($actual) === false;
+    }
+
+    public function matchFalsey($actual)
+    {
+        $this->actual = $actual;
+
+        if (is_bool($this->actual)) {
+            return $this->actual !== false;
+        }
+
+        return isset($this->actual);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reportFailed(FailedMessage $message)
+    {
+        $message->appendText('Expected falsey value, got ')
+            ->appendValue($this->actual);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reportNegativeFailed(FailedMessage $message)
+    {
+        $message->appendText('Expected truthy value, got ')
+            ->appendValue($this->actual);
+    }
+
+}
