2265fc27320ad4a129881c05f0c86b04caca5ab5
Noritaka Horio <holy.shared.design@gmail.com>
2015-03-27 02:34:54 +0000

    Add doc comment

--- src/MatcherFactory.php
+++ src/MatcherFactory.php
@@ -11,7 +11,21 @@
 
 namespace expect;
 
+/**
+ * Factory of matcher.
+ *
+ * @author Noritaka Horio <holy.shared.design@gmail.com>
+ * @copyright Noritaka Horio <holy.shared.design@gmail.com>
+ */
 interface MatcherFactory
 {
+    /**
+     * Create a matcher.
+     *
+     * @param string $name      matcher name
+     * @param array  $arguments parameters for create
+     *
+     * @return \expect\matcher\ReportableMatcher
+     */
     public function create($name, array $arguments = []);
 }
